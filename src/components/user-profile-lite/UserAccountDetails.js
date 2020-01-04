87f15bedc3435c895aee502554f6f4264d973a2b
Anas-MI <54795271+Anas-MI@users.noreply.github.com>
2020-01-04 12:45:43 +0530

    Validations added

--- src/components/user-profile-lite/UserAccountDetails.js
+++ src/components/user-profile-lite/UserAccountDetails.js
@@ -1,8 +1,15 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import apiList from "../../services/apis/apiList";
 import axios from "axios";
-
+import { Tag } from "antd";
+import {
+  validateEmail,
+  validateName,
+  validateNumber,
+  validateState,
+  validateCountry
+} from "../../utils/validations";
 import {
   Card,
   CardHeader,
@@ -15,237 +22,217 @@ import {
   FormInput,
   FormSelect,
   FormTextarea,
-  Button
+  Button,
+  FormFeedback
 } from "shards-react";
 
-export default class UserAccountDetails extends Component{
-  
-    constructor(props){
-      super(props)
-      this.state = {
-        userData: {},
-        fields: {},
-        errors: {}
-      }
-      
-    }
-
-
-    componentDidMount(){
-      const { updateUserDetailsApi, getUserDetailsApi } = apiList;
-      const getUserDetailsRoute =
-        getUserDetailsApi + `${this.props.slug}`;
-      axios
-        .get(getUserDetailsRoute)
-        .then(res => {
-          if (res.status === 200) {
-            this.setState({ ...res.data });
-          }
-        })
-        .catch(err => {
-          console.log({ "Something went wrong": err });
-        });
-      // axios.post(updateUserDetailsApi, userData).then().catch()
-    }
-
-  handleValidation(){
-   let fields = this.state.fields;
-   let errors = {};
-   let formIsValid = true;
-  
-   //Name
-   if(!fields["name"]){
-      formIsValid = false;
-      errors["name"] = "Cannot be empty";
-   }
-  
-   if(typeof fields["name"] !== "undefined"){
-      if(!fields["name"].match(/^[a-zA-Z]+$/)){
-         formIsValid = false;
-         errors["name"] = "Only letters";
-      }        
-   }
-  
-   //Email
-   if(!fields["email"]){
-      formIsValid = false;
-      errors["email"] = "Cannot be empty";
-   }
-  
-   if(typeof fields["email"] !== "undefined"){
-      let lastAtPos = fields["email"].lastIndexOf('@');
-      let lastDotPos = fields["email"].lastIndexOf('.');
-  
-      if (!(lastAtPos < lastDotPos && lastAtPos > 0 && fields["email"].indexOf('@@') == -1 && lastDotPos > 2 && (fields["email"].length - lastDotPos) > 2)) {
-         formIsValid = false;
-         errors["email"] = "Email is not valid";
-       }
-  }  
-  
-  this.setState({errors: errors});
-  return formIsValid;
+export default class UserAccountDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fields: {},
+      errors: {},
+      nameError: false,
+      emailError: false,
+      device: ""
+    };
   }
 
+  componentDidMount() {
+    const { updateUserDetailsApi, getUserDetailsApi } = apiList;
+    const getUserDetailsRoute = getUserDetailsApi + `${this.props.slug}`;
+    axios
+      .get(getUserDetailsRoute)
+      .then(res => {
+        if (res.status === 200) {
+          this.setState({ ...res.data });
+        }
+      })
+      .catch(err => {
+        console.log({ "Something went wrong": err });
+      });
+    // axios.post(updateUserDetailsApi, userData).then().catch()
+  }
 
+  handleChange(name, e) {
+    var change = {};
+    change[name] = e.target.value;
+    this.setState(change);
 
+    //Validations
 
+    //Checking for name
 
-contactSubmit(e){
-e.preventDefault();
+    if (name === "name") {
+      //Returns nameError true or false depending upon the input
 
-if(this.handleValidation()){
-  alert("Form submitted");
-}else{
-  alert("Form has errors.")
-}
+      this.setState(validateName(e.target.value));
+    }
 
-}
+    //Checking for email
 
-handleChange(name, e){         
-// let fields = this.state.fields;
-// fields[field] = e.target.value;        
-// this.setState({fields});
-var change = {};
-change[name] = e.target.value;
-this.setState(change);
+    if (name === "email") {
+      //Returns emailError true or false depending upon the input
 
+      this.setState(validateEmail(e.target.value));
+    }
 
-}
+    //Checking for phone number
+    if (name === "number") {
+      if (name.length > 0) {
+        this.setState({ validateNumber: true });
+        this.setState(validateNumber(e.target.value));
+      } else {
+        this.setState({ validateNumber: false });
+      }
+    }
+  }
 
-  render(){
-    const { title, user} = this.props;
+  // submitForm(e){
+  //  e.preventDefault();
+  //  const data = {
+  //   firstName: this.state.firstName,
+  //   lastName: this.state.lastName,
+  //   email: this.state.email,
+  //   password: this.state.password
+  //  }
+  //  sendFormData(data).then(res=>{
+  //    if(res.status===200){
+  //      alert(res.data);
+  //      this.props.history.push('/');
+  //    }else{
 
-console.log(this.state)
+  //    }
+  //  });
+  // }
 
-    return(
+  render() {
+    const { title, user } = this.props;
 
-      
+    console.log(this.state);
+
+    return (
       <Card small className="mb-4">
-    <CardHeader className="border-bottom">
-      <h6 className="m-0">{title}</h6>
-    </CardHeader>
-    <ListGroup flush>
-      <ListGroupItem className="p-3">
-        <Row>
-          <Col>
-            <Form>
-              <Row form>
-                {/* First Name */}
-                <Col md="6" className="form-group">
-                  <label htmlFor="feName">Name</label>
-                  <FormInput
-                    id="feName"
-                    placeholder="Name"
-                    onChange={this.handleChange.bind(this, "name")} value={this.state.name}
-                    />
-                </Col>
-                {/* Last Name */}
-                <Col md="6" className="form-group">
-                  <label htmlFor="feLastName">Phone Number</label>
-                  <FormInput
-                    id="fePhoneNumber"
-                    placeholder="Phone Number"
-                    value={user.phone}
-                    type="number"
-                    onChange={this.handleChange.bind(this, "number")} value={this.state.number}
-                    />
-                </Col>
-              </Row>
-              <Row form>
-                {/* Email */}
-                <Col md="6" className="form-group">
-                  <label htmlFor="feEmail">Email</label>
-                  <FormInput
-                    type="email"
-                    id="feEmail"
-                    placeholder="Email Address"
-                    value={this.state.email}
-                    onChange={this.handleChange.bind(this, "email")} 
-                    autoComplete="email"
-                  />
-                </Col>
-                {/* Password */}
-                {/* <Col md="6" className="form-group">
-                  <label htmlFor="fePassword">Password</label>
-                  <FormInput
-                  type="password"
-                    id="fePassword"
-                    placeholder="Password"
-                    value=""
-                    onChange={() => {}}
-                    autoComplete="current-password"
-                    />
-                  </Col> */}
-              </Row>
-              <FormGroup>
-                <label htmlFor="feAddress">Address</label>
-                <FormInput
-                  id="feAddress"
-                  placeholder="Address"
-                  value="1234 Main St."
-                  onChange={this.handleChange.bind(this, "address")} 
-                  />
-              </FormGroup>
-              <Row form>
-                {/* City */}
-                <Col md="6" className="form-group">
-                  <label htmlFor="feCity">Country</label>
-                  <FormInput
-                    id="feCity"
-                    placeholder="Country"
-                    value={this.state.country}
-                    onChange={this.handleChange.bind(this, "country")} 
-                    />
-                </Col>
-                {/* State */}
-                <Col md="4" className="form-group">
-                  <label htmlFor="feInputState">State</label>
-                  <FormInput
-                    id="feCity"
-                    placeholder="state"
-                    value={this.state.state}
-                    onChange={this.handleChange.bind(this, "state")} 
+        <CardHeader className="border-bottom">
+          <h6 className="m-0">{title}</h6>
+        </CardHeader>
+        <ListGroup flush>
+          <ListGroupItem className="p-3">
+            <Row>
+              <Col>
+                <Form>
+                  <Row form>
+                    {/* First Name */}
+                    <Col md="6" className="form-group">
+                      <label htmlFor="feName">Name</label>
+                      <FormInput
+                        id="feName"
+                        placeholder="Name"
+                        valid={!this.state.nameError}
+                        invalid={this.state.nameError}
+                        onChange={this.handleChange.bind(this, "name")}
+                        value={this.state.name}
+                      />
+                      <FormFeedback valid>Name looks good!</FormFeedback>
+                      <FormFeedback>
+                        Oops, {this.state.nameErrorMsg}
+                      </FormFeedback>
+                    </Col>
+                    {/* Last Name */}
+                    <Col md="6" className="form-group">
+                      <label htmlFor="feLastName">Phone Number</label>
+                      <FormInput
+                        id="fePhoneNumber"
+                        placeholder="Phone Number"
+                        value={user.phone}
+                        type="number"
+                        maximum={12}
+                        valid={
+                          !this.state.numberError && this.state.validateNumber
+                        }
+                        invalid={this.state.numberError}
+                        onChange={this.handleChange.bind(this, "number")}
+                        value={this.state.number}
+                      />
+                      <FormFeedback valid>Number looks good!</FormFeedback>
+                      <FormFeedback>
+                        Oops, {this.state.numberErrorMsg}
+                      </FormFeedback>
+                    </Col>
+                  </Row>
+                  <Row form>
+                    {/* Email */}
+                    <Col md="6" className="form-group">
+                      <label htmlFor="feEmail">Email</label>
+                      <FormInput
+                        type="email"
+                        id="feEmail"
+                        placeholder="Email Address"
+                        valid={!this.state.emailError}
+                        invalid={this.state.emailError}
+                        value={this.state.email}
+                        onChange={this.handleChange.bind(this, "email")}
+                        autoComplete="email"
+                      />
+                      <FormFeedback valid>Email looks good!</FormFeedback>
+                      <FormFeedback>
+                        Oops, {this.state.emailErrorMsg}
+                      </FormFeedback>
+                    </Col>
+                    <Col md="1" className="form-group"></Col>
+                    <Col
+                      md="5"
+                      align-center
+                      style={{ marginTop: "33px" }}
+                      className="form-group"
+                    >
+                      <span>Device Type : </span>
+                      {this.state.device.includes("ios") && (
+                        <Tag color="magenta">Ios</Tag>
+                      )}
+                      {this.state.device.includes("android") && (
+                        <Tag color="cyan">Android</Tag>
+                      )}
+                    </Col>
+                  </Row>
 
-                    />
-                </Col>
-                {/* Zip Code */}
-                <Col md="2" className="form-group">
-                  <label htmlFor="feZipCode">Zip</label>
-                  <FormInput
-                    id="feZipCode"
-                    placeholder="Zip"
-                    onChange={this.handleChange.bind(this, "zip")} 
-                    
-                  />
-                </Col>
-              </Row>
-              <Row form>
-                {/* Description */}
-                <Col md="12" className="form-group">
-                  <label htmlFor="feDescription">Description</label>
-                  <FormTextarea id="feDescription" rows="5" />
-                </Col>
-              </Row>
-              <Button theme="accent">Update Account</Button>
-            </Form>
-          </Col>
-        </Row>
-      </ListGroupItem>
-    </ListGroup>
-  </Card>
-)
+                  <Row form>
+                    <Col md="6" className="form-group">
+                      <label htmlFor="feCity">Country</label>
+                      <FormInput
+                        id="feCity"
+                        placeholder="Country"
+                        value={this.state.country}
+                        onChange={this.handleChange.bind(this, "country")}
+                      />
+                    </Col>
+
+                    <Col md="4" className="form-group">
+                      <label htmlFor="feInputState">State</label>
+                      <FormInput
+                        id="feCity"
+                        placeholder="state"
+                        value={this.state.state}
+                        onChange={this.handleChange.bind(this, "state")}
+                      />
+                    </Col>
+                  </Row>
+                  <Row form></Row>
+                  <Button theme="accent">Update Account</Button>
+                </Form>
+              </Col>
+            </Row>
+          </ListGroupItem>
+        </ListGroup>
+      </Card>
+    );
+  }
 }
-};
 
 UserAccountDetails.propTypes = {
-  /**
-   * The component's title.
-   */
   title: PropTypes.string
 };
 
 UserAccountDetails.defaultProps = {
   title: "Account Details"
 };
-
-
---
+++ src/utils/validations.js
@@ -0,0 +1,104 @@
+// Email validations
+
+export const validateEmail = email => {
+  if (email === "" || email === null) {
+    return {
+      emailError: true,
+      emailErrorMsg: "Email can't be empty"
+    };
+  } else {
+    const pattern = /[a-zA-Z0-9]+[\.]?([a-zA-Z0-9]+)?[\@][a-z]{3,9}[\.][a-z]{2,5}/g;
+    const result = pattern.test(email);
+    if (result === true) {
+      return {
+        emailError: false,
+        emailErrorMsg: ""
+      };
+    } else {
+      return {
+        emailError: true,
+        emailErrorMsg: "Email is Not Valid"
+      };
+    }
+  }
+};
+
+//Name validations
+export const validateName = name => {
+  if (name === "" || name === null) {
+    return {
+      nameError: true,
+      nameErrorMsg: "Name can't be empty!"
+    };
+  } else {
+    const pattern = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/g;
+    const result = pattern.test(name.trim());
+
+    if (result === true) {
+      return {
+        nameError: false,
+        nameErrorMsg: ""
+      };
+    } else {
+      return {
+        nameError: true,
+        nameErrorMsg: "Name doesn't seems valid"
+      };
+    }
+  }
+};
+
+//Country validation
+export const validateCountry = country => {
+  if (country === "" || country === null) {
+    return {
+      countryError: true,
+      countryErrorMsg: "Country can't be empty!"
+    };
+  } else {
+    return {
+      countryError: false,
+      countryErrorMsg: ""
+    };
+  }
+};
+
+//State validation
+export const validateState = state => {
+  if (state === "" || state === null) {
+    return {
+      stateError: true,
+      stateErrorMsg: "Name can't be empty!"
+    };
+  } else {
+    return {
+      stateError: false,
+      stateErrorMsg: ""
+    };
+  }
+};
+
+//Phone Number validation
+export const validateNumber = number => {
+    if(number.length>12){
+        return {
+            numberError: true,
+            numberErrorMsg: "Phone number cant be more than 12 digits"
+          };
+    } else {
+
+  const pattern = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/;
+  const result = pattern.test(number.trim());
+  console.log({ result });
+  if (result === true) {
+    return {
+      numberError: false,
+      numberErrorMsg: ""
+    };
+  } else {
+    return {
+      numberError: true,
+      numberErrorMsg: "Number doesn't seems valid"
+    };
+  }
+}};
