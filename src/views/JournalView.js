1e23a55157ab7548eaab459d998f1e22cc103f24
Anas-MI <54795271+Anas-MI@users.noreply.github.com>
2020-01-07 17:01:32 +0530

    fix

--- src/views/JournalView.js
+++ src/views/JournalView.js
@@ -1,20 +1,13 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import Highlighter from "react-highlight-words";
 import { Row, Container, Col } from "shards-react";
 import PageTitle from "../components/common/PageTitle";
-import { Table, Input, Button, Icon, Tag, Divider,Popconfirm } from "antd";
-import { Modal } from 'antd';
+import { Table, Input, Button, Icon,  Divider, Popconfirm } from "antd";
+import { Modal } from "antd";
 import apiList from "../services/apis/apiList";
 import axios from "axios";
 const { journalGetAllApi } = apiList;
 const { TextArea } = Input;
-
-//Modal
-
-
-
-
 
 
 
@@ -28,7 +21,7 @@ export default class Synergistic extends Component {
       visible: false,
       confirmLoading: false,
       disabled: true,
-      question:""
+      question: ""
     };
   }
 
@@ -36,57 +29,51 @@ export default class Synergistic extends Component {
     var change = {};
     change[name] = e.target.value;
     this.setState(change);
-    
-    if(this.state.question.length < 3){
-        this.setState({disabled: true})
-} else if(this.state.question.length > 3){
-    this.setState({disabled: false})
 
-}
-  
+    if (this.state.question.length < 3) {
+      this.setState({ disabled: true });
+    } else if (this.state.question.length > 3) {
+      this.setState({ disabled: false });
+    }
   }
 
   showModal = () => {
     this.setState({
-      visible: true,
+      visible: true
     });
   };
 
   handleOk = () => {
     this.setState({
-      ModalText: 'Saving...',
-      confirmLoading: true,
+      ModalText: "Saving...",
+      confirmLoading: true
     });
     setTimeout(() => {
+      const { journalCreateApi } = apiList;
 
-           const { journalCreateApi } = apiList;
-
-        let {question} = this.state
-    axios
-      .post(journalCreateApi, {question})
-      .then(res => {
-        if (res.status === 200) {
-          console.log(res)
-          this.setState({
-            visible: false,
-            confirmLoading: false,
-          });
-        }
-        window.location.reload();
-      })
-      .catch(err => {
-        console.log({ err });
-      });
-
-
-    
+      let { question } = this.state;
+      axios
+        .post(journalCreateApi, { question })
+        .then(res => {
+          if (res.status === 200) {
+       
+            this.setState({
+              visible: false,
+              confirmLoading: false
+            });
+          }
+          window.location.reload();
+        })
+        .catch(err => {
+          console.log({ err });
+        });
     }, 1500);
   };
 
   handleCancel = () => {
-    console.log('Clicked cancel button');
+
     this.setState({
-      visible: false,
+      visible: false
     });
   };
 
@@ -95,18 +82,14 @@ export default class Synergistic extends Component {
       .get(journalGetAllApi)
       .then(res => {
         if (res.status === 200) {
-          
           if (res.data) {
             this.journalList = res.data.data.map((journal, index) => ({
               key: index + 1,
               question: journal.question ? journal.question : "-",
               id: journal._id ? journal._id : "Not Found"
-
             }));
 
             this.setState({ journalList: this.journalList });
-            
-          
           }
         }
       })
@@ -195,19 +178,17 @@ export default class Synergistic extends Component {
   };
 
   confirm(record, b) {
-      
     const { journalCommonApi } = apiList;
     const journalCommonApiUrl = journalCommonApi + "/delete/" + record.id;
-    console.log(journalCommonApiUrl)
+
     axios
       .delete(journalCommonApiUrl)
       .then(res => {
-        console.log(res)
+
 
         if (res.status === 200) {
-          
           setTimeout(() => {
-            window.location.reload()
+            window.location.reload();
           }, 1000);
         }
       })
@@ -217,9 +198,7 @@ export default class Synergistic extends Component {
   }
 
   render() {
-
     const { visible, confirmLoading, ModalText } = this.state;
-   
 
     const columns = [
       {
@@ -235,65 +214,35 @@ export default class Synergistic extends Component {
         key: "question",
         // width: "20%",
         ...this.getColumnSearchProps("question")
-      },
-    //   {
-    //     title: "Goal",
-    //     dataIndex: "goal",
-    //     key: "goal",
-    //     ...this.getColumnSearchProps("goal")
-    //   },
-    //   {
-    //     title: "Week",
-    //     dataIndex: "week",
-    //     key: "week",
-    //     sorter: (a, b) => a.key - b.key,
-    //     ...this.getColumnSearchProps("week")
-    //   },
-    //   {
-    //     title: "Day",
-    //     dataIndex: "day",
-    //     key: "day",
-    //     sorter: (a, b) => a.day.length - b.day.length,
-    //     render: tag => {
-    //       return (
-    //         <span>
-    //           <Tag color="volcano" key={tag}>
-    //             {tag.toUpperCase()}
-    //           </Tag>
-    //         </span>
-    //       );
-    //     }
-    //   },
+      }
       {
         title: "Action",
         dataIndex: "",
         key: "x",
         render: (text, record) => (
-            <div>
-                   <Popconfirm
-                    placement="right"
-                    title="Are you sure you want to delete this Question?"
-                    onConfirm={this.confirm.bind(this, record)}
-                    okText="Yes"
-                    cancelText="No"
-                  >
-          <Divider type="vertical"/>
-           {/* <Link to={`/delete/synergy/${record.id}`}>Delete</Link> */}
-        
-            <Button
-                      style={{     height: "50px" }}
-                    //   theme="danger"
-                    className=" bg-danger text-white text-center rounded p-3"
+          <div>
+            <Popconfirm
+              placement="right"
+              title="Are you sure you want to delete this Question?"
+              onConfirm={this.confirm.bind(this, record)}
+              okText="Yes"
+              cancelText="No"
+            >
+              <Divider type="vertical" />
 
-                    //   onClick={this.deletePost.bind(this)}
-                    >
-                      <i className="material-icons ">delete</i>Delete Question
-                    </Button>
-          <Divider type="vertical"/>
 
-                  </Popconfirm>
-       
-            </div>
+              <Button
+                style={{ height: "50px" }}
+   
+                className=" bg-danger text-white text-center rounded p-3"
+
+         
+              >
+                <i className="material-icons ">delete</i>Delete Question
+              </Button>
+              <Divider type="vertical" />
+            </Popconfirm>
+          </div>
         )
       }
     ];
@@ -307,41 +256,45 @@ export default class Synergistic extends Component {
             className="ml-sm-auto mr-sm-auto"
           />
           <Col className="md-4">
-          {/* <Link to={`/create/journal`}> */}
-            
-            
-          <div
-        className=" bg-dark text-white text-center rounded p-3"
-        onClick={this.showModal}
-        style={{ boxShadow: "inset 0 0 5px rgba(0,0,0,.2)", height: "50px", width: "237px", marginLeft: "261px"}}>
-        Add New
-      </div>
-            {/* </Link>  */}
+          
+
+            <div
+              className=" bg-dark text-white text-center rounded p-3"
+              onClick={this.showModal}
+              style={{
+                boxShadow: "inset 0 0 5px rgba(0,0,0,.2)",
+                height: "50px",
+                width: "237px",
+                marginLeft: "261px"
+              }}
+            >
+              Add New
+            </div>
+       
             <Modal
-          title="Add a New Question"
-          visible={visible}
-          onOk={this.handleOk}
-          confirmLoading={confirmLoading}
-          onCancel={this.handleCancel}
-          okText="Create"
-           okButtonProps= {{
-      disabled: this.state.disabled
-    }}
-        >
-            <Col md="12" className="form-group">
-        {/* <p>{ModalText}</p> */}
-        <label htmlFor="feQuestion">Question</label>
+              title="Add a New Question"
+              visible={visible}
+              onOk={this.handleOk}
+              confirmLoading={confirmLoading}
+              onCancel={this.handleCancel}
+              okText="Create"
+              okButtonProps={{
+                disabled: this.state.disabled
+              }}
+            >
+              <Col md="12" className="form-group">
+       
+                <label htmlFor="feQuestion">Question</label>
 
-        <TextArea
-          placeholder="Your Question..  "
-          onChange={this.handleChange.bind(this, "question")}
-          value={this.state.question}
-          required={true}
-          autoSize
-        />
-      </Col>
-        </Modal>
-
+                <TextArea
+                  placeholder="Your Question..  "
+                  onChange={this.handleChange.bind(this, "question")}
+                  value={this.state.question}
+                  required={true}
+                  autoSize
+                />
+              </Col>
+            </Modal>
           </Col>
         </Row>
         <Table columns={columns} dataSource={this.state.journalList} />
@@ -349,6 +302,3 @@ export default class Synergistic extends Component {
     );
   }
 }
-
-
-
