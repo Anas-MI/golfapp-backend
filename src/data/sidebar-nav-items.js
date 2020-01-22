ce16d40273ee9a916899c9e134fdbf61a8c518fc
thirdessential <thirdessential@gmail.com>
2020-01-22 14:55:14 +0530

    nav bar fixed

--- src/data/sidebar-nav-items.js
+++ src/data/sidebar-nav-items.js
@@ -47,30 +47,30 @@ export default function() {
       htmlBefore: '<i class="material-icons">table_chart</i>',
       to: "/shipping",
     },
-    {
-      title: "Blog Posts",
-      htmlBefore: '<i class="material-icons">vertical_split</i>',
-      to: "/blog-posts",
-    },
-    {
-      title: "Add New Post",
-      htmlBefore: '<i class="material-icons">note_add</i>',
-      to: "/add-new-post",
-    },
-    {
-      title: "Forms & Components",
-      htmlBefore: '<i class="material-icons">view_module</i>',
-      to: "/components-overview",
-    },
-    {
-      title: "User Profile",
-      htmlBefore: '<i class="material-icons">person</i>',
-      to: "/user-profile-lite",
-    },
-    {
-      title: "Errors",
-      htmlBefore: '<i class="material-icons">error</i>',
-      to: "/errors",
-    }
+    // {
+    //   title: "Blog Posts",
+    //   htmlBefore: '<i class="material-icons">vertical_split</i>',
+    //   to: "/blog-posts",
+    // },
+    // {
+    //   title: "Add New Post",
+    //   htmlBefore: '<i class="material-icons">note_add</i>',
+    //   to: "/add-new-post",
+    // },
+    // {
+    //   title: "Forms & Components",
+    //   htmlBefore: '<i class="material-icons">view_module</i>',
+    //   to: "/components-overview",
+    // },
+    // {
+    //   title: "User Profile",
+    //   htmlBefore: '<i class="material-icons">person</i>',
+    //   to: "/user-profile-lite",
+    // },
+    // {
+    //   title: "Errors",
+    //   htmlBefore: '<i class="material-icons">error</i>',
+    //   to: "/errors",
+    // }
   ];
 }
